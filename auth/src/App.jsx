38e05550af5c17416c0f52a28b7d7db607541da2
Shrishti Namdeo <shrishti.namdeo@gmail.com>
2025-06-20 01:34:01 +0530

    Add a new login route

--- auth/src/App.jsx
+++ auth/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Signup />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/reset-password" element={<ResetPassword />} />
